719ba1b8a2f338323ccbd869c4282c8c51a5cecc
nichgalzin <nichgalzin@gmail.com>
2023-10-10 17:28:54 +0100

    Form button functionality and basic css
    Relates #6, Relates #5, Closes #19

--- src/routes/draw.js
+++ src/routes/draw.js
@@ -3,15 +3,22 @@ const router = express.Router();
 
 const drawImage = require('../templates/drawImage');
 
-
 router.get('/', (req, res) => {
-    try {
-      res.send(drawImage());
-    } catch (err) {
-      console.log(err);
-      throw err;
-    }
-  });
+  try {
+    res.send(drawImage());
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+});
 
-  
+router.get('/back-button', (req, res) => {
+  try {
+    res.redirect('/');
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+});
+
 module.exports = router;
