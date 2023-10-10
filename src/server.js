caf13b92cd859f7115ae589e00381d82431bd880
GeorgeKlemperer <george@klemperer.co.uk>
2023-10-10 17:21:26 +0100

    image saved and page redirected

--- src/server.js
+++ src/server.js
@@ -8,23 +8,25 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const homeRoutes = require('./routes/home.js');
 const drawRoutes = require('./routes/draw.js');
+const saveRoutes = require('./routes/save.js');
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(staticHandler);
 app.use('/', homeRoutes);
 app.use('/draw', drawRoutes);
+app.use('/save', saveRoutes);
 
-app.post('/save', (req, res) => {
-  try {
-    const data = req.body.image;
-    res.json({ success: true });
-    //model handling
-  } catch(error) {
-    console.log(error);
-    throw error;
-    //More meaningful error handling
-  }
-});
+// app.post('/save', (req, res) => {
+//   try {
+//     const data = req.body.image;
+//     res.json({ success: true });
+//     //model handling
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//     //More meaningful error handling
+//   }
+// });
 
 module.exports = app;
