c00b32a3c5c1a9169219c798ec0f7fcc19103cde
nichgalzin <nichgalzin@gmail.com>
2023-10-10 14:40:06 +0100

    Basic canvas setup and fetch request
    Relates #2

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,1 +1,22 @@
-//home route here
+const express = require('express');
+const router = express.Router();
+const comicHome = require('../templates/comicHome');
+
+router.get('/', (req, res) => {
+  try {
+    res.send(comicHome());
+
+  } catch(err) {
+    console.log(err);
+    throw err;
+  }
+});
+
+// router.post('/save', (req, res) => {
+//   const dataURL = req.body.image;
+//   const base64Data = dataURL.replace(/^data:image\/png;base64,/, ''); // Remove data prefix
+//   const binaryData = Buffer.from(base64Data, 'base64');
+//   console.log(binaryData, dataURL);
+// });
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,1 +1,28 @@
-//Server code here
+//Server
+const express = require('express');
+const staticHandler = express.static('public');
+const bodyParser = require('body-parser');
+const fs = require('node:fs');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const homeRoutes = require('./routes/home');
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(staticHandler);
+app.use('/', homeRoutes);
+
+app.post('/save', (req, res) => {
+  try {
+    const data = req.body.image;
+    res.json({ success: true });
+    //model handling
+  } catch(error) {
+    console.log(error);
+    throw error;
+    //More meaningful error handling
+  }
+});
+
+module.exports = app;
